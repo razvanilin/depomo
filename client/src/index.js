d8361b6ab5bd2dbde9ef8eaaa1f3a8549c84da6e
Razvan Ilin <razvanilin@gmail.com>
2017-02-11 21:21:50 +0000

    added jumpsit for the react app

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Rerender } from 'jumpsuit'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import index from './root'
+export default index
+
+if (module.hot) {
+  module.hot.accept('./root', Rerender)
+}
