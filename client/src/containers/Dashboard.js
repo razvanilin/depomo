fe3977ae2d3e598519839e4038c2b4847989dea8
Razvan Ilin <razvanilin@gmail.com>
2017-03-19 12:19:26 +0100

    modified the dashboard to be fully responsive on mobile

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -13,13 +13,17 @@ import Title from 'grommet/components/Title'
 import Image from 'grommet/components/Image'
 import Menu from 'grommet/components/Menu'
 import Split from 'grommet/components/Split'
+import Button from 'grommet/components/Button'
 
 import UserIcon from 'grommet/components/icons/base/User'
 import HistoryIcon from 'grommet/components/icons/base/History'
 import TrophyIcon from 'grommet/components/icons/base/Trophy'
 import PlanIcon from 'grommet/components/icons/base/Plan'
+import CloseIcon from 'grommet/components/icons/base/Close'
+import MenuIcon from 'grommet/components/icons/base/Menu'
 
 export default Component({
+
   componentWillMount() {
     if (window.location.pathname === "/dashboard") {
       Goto({
@@ -31,39 +35,107 @@ export default Component({
         path: "/login"
       });
     }
+
+    this.state = {showMenu: true, responsive: 'multiple'};
   },
+
+  _onResponsive(responsive) {
+    this.setState({responsive: responsive});
+    if ('multiple' === responsive) {
+      this.setState({showMenu: true});
+    }
+    if ('single' === responsive) {
+      this.setState({showMenu: false});
+    }
+  },
+
+  _onMenuOpen () {
+    this.setState({showMenu: true});
+    // this._renderMenu();
+    console.log("On Menu Open");
+  },
+
+  _onMenuClick () {
+    if ('single' === this.state.responsive) {
+      this.setState({showMenu: false});
+    }
+    //this.refs.doc.scrollTop = 0;
+  },
+
+  _renderTitle (invert) {
+    if (!invert) {
+      return (
+        <Title>
+          <Image src={LogoImage} fit="contain" full="horizontal"/>
+        </Title>
+      );
+    } else {
+      return;
+    }
+  },
+
+  _renderMenu() {
+    const title = this._renderTitle(false);
+    let closer;
+    if ('single' === this.state.responsive) {
+      closer = (
+        <Button icon={<CloseIcon />} onClick={this._onMenuClick} />
+      );
+    }
+    return (
+      <Sidebar colorIndex="accent-1" fixed={true} size="small">
+        <Header pad="medium" justify="between">
+          {title}
+          {closer}
+        </Header>
+
+        <Box flex='grow' pad="small">
+          <Menu primary={true}>
+            <Anchor label="Activities" icon={<PlanIcon />} animateIcon={true} href="" className="active"/>
+            <Anchor label="Profile" icon={<UserIcon />} animateIcon={true} href="" />
+            <Anchor label="History" icon={<HistoryIcon />} animateIcon={true} href="" />
+            <Anchor label="Achievements" icon={<TrophyIcon />} animateIcon={true} href="" />
+          </Menu>
+        </Box>
+
+        <Footer pad="medium">
+          <UserIcon />
+          <Menu responsive={true} inline={false} label={this.props.user.name} icon={<UserIcon />} primary={true}>
+            <Anchor href="">Profile Settings</Anchor>
+            <Anchor href="">Log Out</Anchor>
+          </Menu>
+        </Footer>
+      </Sidebar>
+    )
+  },
+
+  _renderDoc () {
+    let header;
+    if ('single' === this.state.responsive) {
+     header = (
+       <Header justify='between' size='medium' pad={{horizontal: 'medium'}} fixed={true}>
+         <Menu direction='row' responsive={false}>
+           <Anchor label="Menu" animateIcon={true} icon={<MenuIcon />} onClick={this._onMenuOpen} />
+         </Menu>
+       </Header>
+     );
+    }
+    return (
+      <Box flex="grow" pad="medium">
+        {header}
+        {this.props.children}
+      </Box>
+    );
+  },
+
   render() {
+    let priority = ('single' === this.state.responsive && this.state.showMenu ?
+      'left' : 'right');
     return (
       <App centered={false}>
-        <Split flex="right">
-          <Sidebar colorIndex="accent-1" fixed={true} size="small">
-            <Header pad="medium" justify="between">
-              <Title>
-                <Image src={LogoImage} fit="contain" full="horizontal"/>
-              </Title>
-            </Header>
-
-            <Box flex='grow' pad="small">
-              <Menu primary={true}>
-                <Anchor label="Activities" icon={<PlanIcon />} animateIcon={true} href="" className="active"/>
-                <Anchor label="Profile" icon={<UserIcon />} animateIcon={true} href="" />
-                <Anchor label="History" icon={<HistoryIcon />} animateIcon={true} href="" />
-                <Anchor label="Achievements" icon={<TrophyIcon />} animateIcon={true} href="" />
-              </Menu>
-            </Box>
-
-            <Footer pad="medium">
-              <UserIcon />
-              <Menu responsive={true} inline={false} label={this.props.user.name} icon={<UserIcon />} primary={true}>
-                <Anchor href="">Profile Settings</Anchor>
-                <Anchor href="">Log Out</Anchor>
-              </Menu>
-            </Footer>
-          </Sidebar>
-
-          <Box flex="grow" pad="medium">
-            {this.props.children}
-          </Box>
+        <Split flex="right" priority={priority} onResponsive={this._onResponsive} fixed={true}>
+          {this._renderMenu()}
+          {this._renderDoc()}
         </Split>
       </App>
     )
