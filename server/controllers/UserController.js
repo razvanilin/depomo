700592e8e07d092fb81288e843d3ff25bb90db27
Razvan Ilin <razvanilin@gmail.com>
2017-03-31 12:55:45 +0100

    completed the back-end code for password reset

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -22,16 +22,6 @@ module.exports = (app, route) => {
     return res.status(200).send("GET /user/:id works");
   });
 
-  app.get('/mandrill', (req, res) => {
-    app.mandrill.users.ping(resource => {
-      console.log(resource);
-      return res.status(200).send(resource);
-    }, err => {
-      console.log(err);
-      return res.status(400).send(err);
-    })
-  });
-
   /** Route to create a user **/
   app.post('/user', (req, res) => {
     if (!req.body.email) return res.status(400).send("Request is missing the email field.");
@@ -187,82 +177,110 @@ module.exports = (app, route) => {
           return res.status(400).send(err ? err : 'No user with that email');
         }
 
-        // send forgot password email
-        // send user a welcome message
-        var templateName = "depomo-forgot-password";
-        var templateContent = [{
-          name: "Forgot Passowrd",
-          content: "Get ready to break procrastination"
-        }];
-        var message = {
-          from_email: "razvan@depomo.com",
-          from_name: "Razvan",
-          to: [{
-            email: user.email,
-            name: user.name,
-            type: "to"
-          }],
-          headers: {
-            "Reply-To": "razvan@depomo.com"
-          },
-          track_opens: true,
-          track_clicks: true,
-          merge_vars: [{
-            rcpt: user.email,
-            vars: [{
-              name: 'fname',
-              content: user.name
-            }, {
-              name: "resetlink",
-              content: user.resetToken
-            }]
-          }],
-          tags: ["forgot"]
-        }
+        bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+          }
+          // hash the email and user ID for extra security
+          bcrypt.hash(user._id + "-" + user.email, salt, (err, hash) => {
 
-        app.mandrill.messages.sendTemplate({
-          template_name: templateName,
-          template_content: templateContent,
-          message: message,
-          async: true,
-          ip_pool: "Main Pool"
-        }, result => {
-          console.log("email sent");
-          console.log(result);
-        }, err => {
-          console.log("email error");
-          console.log(err);
+            // send forgot password email
+            // send user a welcome message
+            var templateName = "depomo-forgot-password";
+            var templateContent = [{
+              name: "Forgot Passowrd",
+              content: "Get ready to break procrastination"
+            }];
+            var message = {
+              from_email: "razvan@depomo.com",
+              from_name: "Razvan",
+              to: [{
+                email: user.email,
+                name: user.name,
+                type: "to"
+              }],
+              headers: {
+                "Reply-To": "razvan@depomo.com"
+              },
+              track_opens: true,
+              track_clicks: true,
+              merge_vars: [{
+                rcpt: user.email,
+                vars: [{
+                  name: 'fname',
+                  content: user.name
+                }, {
+                  name: "resetlink",
+                  content: app.settings.host + "/reset?token=" + user.resetToken + "&hash=" + hash
+                }]
+              }],
+              tags: ["forgot"]
+            }
+
+            app.mandrill.messages.sendTemplate({
+              template_name: templateName,
+              template_content: templateContent,
+              message: message,
+              async: true,
+              ip_pool: "Main Pool"
+            }, result => {
+              console.log("email sent");
+              console.log(result);
+            }, err => {
+              console.log("email error");
+              console.log(err);
+            });
+
+            return res.status(200).send("Email sent.");
+          });
         });
-
-        return res.status(200).send("Email sent.");
       });
     });
   });
+  // ----------------------------------------------------------
 
-  // function verifyOwner(req, res, next) {
-  //   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  //
-  //   if (token) {
-  //     jwt.verify(token, app.settings.secret, (err, decoded) => {
-  //       if (err) return res.status(401).send("Unauthorized access.");
-  //       User.findOne({
-  //         _id: decoded._doc._id
-  //       }, (err, user) => {
-  //
-  //         if (err) return res.status(400).send("Could not process your user information. Try again later.")
-  //
-  //         if (user._id == req.params.id || user.isAdmin) {
-  //           req.decoded = decoded;
-  //           next();
-  //         } else {
-  //           return res.status(401).send("Not authorized to access resource.");
-  //         }
-  //       });
-  //     });
-  //   } else {
-  //     return res.status(401).send("Token is missing.");
-  //   }
-  // }
+  /** Route to change the password **/
+  app.put("/user/password", (req, res) => {
+    if (!req.body.token || !req.body.hash) return res.status(400).send("Body is missing token or hash.");
+    if (!req.body.password) return res.status(400).send("The password field is missing from the body.");
+
+    User.findOne({resetToken: req.body.token}, (err, user) => {
+      if (err) return res.status(400).send(err);
+      if (!user || user.length < 1) return res.status(404).send("Reset token is not valid");
+
+      // compare the hash with the user information
+      bcrypt.compare(user._id + "-" + user.email, req.body.hash, (err, isMatch) => {
+        if (err) {
+          return res.status(401).send(err);
+        }
+        if (!isMatch) return res.status(401).send("The hash is not valid.");
+
+        bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+          }
+          bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if (err) {
+              console.log(err);
+              return res.status(400).send(err);
+            }
+
+            User.findByIdAndUpdate(user._id, { $set: {password: hash, resetToken: ""}}, {new: true}, (err, user) => {
+              if (err) {
+                console.log(err);
+                return res.status(400).send(err);
+              }
+
+              return res.status(200).send("Password reset");
+            });
+          });
+        });
+      });
+    });
+  });
+  // ----------------------------------------------------------
 
   return (req, res, next) => {
     next();
